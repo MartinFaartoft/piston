1ebb7ae53558f4ffc4db1979e6ccc18ebf881148
Martin Faartoft <martin.faartoft@gmail.com>
2016-06-28 17:14:40 +0200

    removing sample from source control, fixing gitignore

---
+++ src/sample/mouseball.ts
@@ -0,0 +1,36 @@
+/// <reference path="../../dist/piston-0.4.0.d.ts" />
+
+namespace SampleGame {
+    export class MouseBall extends ps.Entity {
+        color: string = "yellow";
+        constructor() {
+            super(new ps.Point(0, 0));
+            this.radius = 5;
+            this.rotationSpeed = -1;
+            this.isCollisionDetectionEnabled = true;
+        }
+
+        update(dt: number, dims: ps.Vector) {
+        this.pos = this.engine.mouse.pos;
+        if (this.engine.mouse.isLeftButtonDown) {
+            this.color = "green";
+        } else if (this.engine.mouse.isRightButtonDown) {
+            this.color = "red";
+        } else if (this.engine.mouse.isMiddleButtonDown) {
+            this.color = "blue";
+        } else {
+            this.color = "yellow";
+        }
+
+        this.rotation += 1 * dt;
+    }
+
+        render(camera: ps.Camera) {
+            camera.fillArc(this, this.radius, 0, Math.PI * 1.2, false, this.color);
+        }
+
+        collideWith(other: ps.Entity) {
+            this.destroyed = true;
+        }
+    }
+}
